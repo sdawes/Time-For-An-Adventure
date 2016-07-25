43dc67a711d2669b3e8438127df165d5841488aa
Thomas Harker <tom.harker@hotmail.co.uk>
2016-07-25 18:25:34 +0100

    Add registrations controller and adventures contoller. Sign up now working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-
-  devise_for :users
-  root to: "adventure#index"
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  root to: "adventures#index"
+  resources :adventures
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
