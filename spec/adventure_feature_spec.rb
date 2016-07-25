a09257abd667f61171db4d7e1bbbe068996d447d
Jonathan <j_passe@live.fr>
2016-07-25 18:11:38 +0100

    populate the adventures table with adventures

--- spec/adventure_feature_spec.rb
+++ spec/adventure_feature_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 feature "adventures" do
   scenario "User can see an adventure" do
-    adventure = create(:adventure)
     visit "/"
     expect(page).to have_content "Makers"
   end
