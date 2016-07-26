68fb16021dbd41a01a249aeafe53655fc805b469
lomlo <michael.lo@live.co.uk>
2016-07-26 15:39:45 +0100

    refactored the user_login_spec to read without context statements

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -2,34 +2,31 @@ require 'rails_helper'
 
 feature 'User authentication' do
 
-  context 'A user without an account' do
-    scenario "can not log in " do
-      @user = build(:user)
+  scenario "A user without an account can not log in " do
+    @user = build(:user)
 
-      sign_in_with @user
+    sign_in_with @user
 
-      expect(page).to have_content('Invalid Email or password.')
-    end
+    expect(page).to have_content('Invalid Email or password.')
   end
 
-  context 'A user with an account' do
-    scenario "can log in " do
-      @user = create(:user)
+  scenario "A user with an account can log in " do
+    @user = create(:user)
 
-      sign_in_with @user
+    sign_in_with @user
 
-      expect(page).to have_content('Signed in successfully.')
-      expect(page).to_not have_content('Sign up')
-      expect(page).to have_content('Log out')
-    end
-
-    scenario 'can log out' do
-      @user = create(:user)
-
-      sign_in_with @user
-      click_link 'Log out'
-
-      expect(page).to have_content('Signed out successfully')
-    end
+    expect(page).to have_content('Signed in successfully.')
+    expect(page).to_not have_content('Sign up')
+    expect(page).to have_content('Log out')
   end
+
+  scenario 'A user with an account can log out' do
+    @user = create(:user)
+
+    sign_in_with @user
+    click_link 'Log out'
+
+    expect(page).to have_content('Signed out successfully')
+  end
+
 end
