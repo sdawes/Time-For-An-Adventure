8f8300c2bffd9caac003f4afa28ad6f8c4158303
lomlo <michael.lo@live.co.uk>
2016-07-26 15:53:32 +0100

    added a html validation test to the user_login_spec, with instructions of how to run with firefox and selinium

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
+# A test is pending but works! - to run use selnium (gemfile) and firefox (version 41.0.1)
 
 feature 'User authentication' do
+  xscenario "html validation requires a user name to log in", js: true do
+    visit '/users/sign_in'
+    fill_in 'Password', with: 'password'
+    click_link 'Log in'
+
+    expect(current_path).to eq '/users/sign_in'
+  end
 
   scenario "A user without an account can not log in " do
     @user = build(:user)
@@ -20,12 +28,12 @@ feature 'User authentication' do
     expect(page).to have_content('Log out')
   end
 
-  scenario 'A user with an account can log out' do
+  scenario "A user with an account can log out" do
     @user = create(:user)
 
     sign_in_with @user
     click_link 'Log out'
 
-    expect(page).to have_content('Signed out successfully')
+    expect(page).to have_content("Signed out successfully")
   end
 end
