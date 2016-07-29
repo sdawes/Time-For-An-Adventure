eef8805f4fb9cae1790afb1c7426dae5f963c836
Jonathan <j_passe@live.fr>
2016-07-29 10:34:40 +0100

    fixing merging issues branch 40

--- app/assets/javascripts/chapters.js
+++
@@ -1,4 +0,0 @@
-$('#choice_button').click(function(event) {
-  console.log(this);
-  var clicked_div = $();
-});
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,11 +1,11 @@
 def sign_in_with(user)
   visit '/'
-  within('.nav_links') do
-    click_button 'Log in'
-  end
+
+  click_link 'Log in'
+
   fill_in 'Email', with: user.email
   fill_in 'Password', with: user.password
-  within('.actions') do
-    click_button 'Log in'
-  end
+
+  click_button 'Log in'
+
 end
