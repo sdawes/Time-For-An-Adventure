4aeb295266bc7d780e8e777bb1174a986a1c5327
Marlon Date-Chong <mdatechong@gmail.com>
2016-07-30 20:44:02 +0100

    adding image to adventure

--- spec/models/adventure_spec.rb
+++ spec/models/adventure_spec.rb
@@ -13,4 +13,28 @@ describe Adventure, type: :model do
   scenario "adventure can be created" do
     expect{create( :adventure, title: "Hello", synopsis: "once upon a time", image: Rack::Test::UploadedFile.new(Rails.root + 'app/assets/images/Bristol.jpeg', 'image/jpeg') )}.to change{Adventure.count}.by(1)
   end
+
+  scenario "non image type files cannot be added" do
+    adventure = 'app/controllers/concerns/.keep'
+    adventure_rb = 'app/controllers/adventures_controller.rb'
+    adventure_html_erb = 'app/views/adventures/index.html.erb'
+    adventure_md = 'README.md'
+
+    expect(Adventure.new(title: "Hello", synopsis: "once upon a time", image: Rack::Test::UploadedFile.new(Rails.root + "#{adventure_rb}", 'image/jpeg'))).to have(1).error_on(:image)
+    expect(Adventure.new(title: "Hello", synopsis: "once upon a time", image: Rack::Test::UploadedFile.new(Rails.root + "#{adventure_html_erb}", 'image/jpeg'))).to have(1).error_on(:image)
+    expect(Adventure.new(title: "Hello", synopsis: "once upon a time", image: Rack::Test::UploadedFile.new(Rails.root + "#{adventure_md}", 'image/jpeg'))).to have(1).error_on(:image)
+    expect(Adventure.new(title: "Hello", synopsis: "once upon a time", image: Rack::Test::UploadedFile.new(Rails.root + "#{adventure}", 'image/jpeg'))).to have(1).error_on(:image)
+  end
+
+  scenario "type of image files accepted" do
+    jpeg = 'app/assets/images/Bristol.jpeg'
+    png = 'app/assets/images/png.png'
+    svg = 'app/assets/images/svg.svg'
+    gif = 'app/assets/images/giphy.gif'
+
+    expect{create( :adventure, title: "jpeg", synopsis: "once upon a time", image: Rack::Test::UploadedFile.new(Rails.root + "#{jpeg}", 'image/jpeg') )}.to change{Adventure.count}.by(1)
+    expect{create( :adventure, title: "png", synopsis: "once upon a time", image: Rack::Test::UploadedFile.new(Rails.root + "#{png}", 'image/jpeg') )}.to change{Adventure.count}.by(1)
+    expect{create( :adventure, title: "svg", synopsis: "once upon a time", image: Rack::Test::UploadedFile.new(Rails.root + "#{svg}", 'image/jpeg') )}.to change{Adventure.count}.by(1)
+    expect{create( :adventure, title: "gif", synopsis: "once upon a time", image: Rack::Test::UploadedFile.new(Rails.root + "#{gif}", 'image/jpeg') )}.to change{Adventure.count}.by(1)
+  end
 end
