67b1fa2564629e475056b0edb301bf8862d61a27
lomlo <michael.lo@live.co.uk>
2016-07-30 15:25:29 +0100

    edited the chapters controller

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -3,11 +3,7 @@ class ChaptersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    if current_user
       @chapter = Chapter.find(params[:id])
       @choices = Choice.where(chapter_id: "#{@chapter.id}")
-    else
-      redirect_to '/'
-    end
   end
 end
