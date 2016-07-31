36ad366f0f651ad45cc84e225c645c90d22cc748
Marlon Date-Chong <mdatechong@gmail.com>
2016-07-31 17:51:59 +0100

    fixing merge conflicts

--- db/migrate/20160729114048_change_column_null.rb
+++ db/migrate/20160729114048_change_column_null.rb
@@ -1,5 +1,6 @@
 class ChangeColumnNull < ActiveRecord::Migration[5.0]
   def change
+    change_column_null :adventures, :image, false
     change_column_null :choices, :option, false
     change_column_null :choices, :resulting_chapter_id, false
     change_column_null :choices, :chapter_id, false
--- db/schema.rb
+++ db/schema.rb
@@ -27,19 +27,19 @@ ActiveRecord::Schema.define(version: 20160730165305) do
   end
 
   create_table "chapters", force: :cascade do |t|
-    t.string   "description",  null: false
+    t.string   "description"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.integer  "adventure_id", null: false
+    t.integer  "adventure_id"
     t.index ["adventure_id"], name: "index_chapters_on_adventure_id", using: :btree
   end
 
   create_table "choices", force: :cascade do |t|
     t.string   "option",               null: false
-    t.integer  "resulting_chapter_id", null: false
+    t.integer  "resulting_chapter_id"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
-    t.integer  "chapter_id",           null: false
+    t.integer  "chapter_id"
     t.index ["chapter_id"], name: "index_choices_on_chapter_id", using: :btree
   end
 
