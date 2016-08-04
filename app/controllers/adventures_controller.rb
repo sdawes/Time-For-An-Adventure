2ac9b14ebd8b475071656c5167c756b283ccd715
Marlon Date-Chong <mdatechong@gmail.com>
2016-08-04 12:17:01 +0100

    fixing creating

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -13,14 +13,22 @@ class AdventuresController < ApplicationController
   end
 
   def create
-    @adventure = Adventure.create(adventure_params)
-    @adventure_id = @adventure.id
-    redirect_to adventure_design_path(@adventure_id)
+    @adventure = Adventure.new(adventure_params)
+    if @adventure.save
+      @adventure_id = @adventure.id
+      redirect_to new_adventure_chapter_path(@adventure)
+    else
+      flash[:error] = "Title needs to be unique"
+      redirect_to new_adventure_path
+    end
   end
 
   def design
     @adventure = Adventure.find(params[:adventure_id])
     @chapters = Chapter.where(adventure_id: @adventure.id)
+    unless @chapters[0]
+      redirect_to new_adventure_chapter_path(@adventure)
+    end
     @adventure_id = @adventure.id
   end
 
